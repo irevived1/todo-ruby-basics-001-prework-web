bf9567ececa0625f747e38fed3116b7d72b04fb0
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-04-29 19:41:52 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
-def division(num1, num2)
- 
+def division(num1=1, num2=1)
+  return num1/num2
 end
 
-def assign_variable(value)
-  
+def assign_variable(value = 1  )
+ return value
 end
 
-def argue
-  
+def argue(a = 1)
+  return a
 end
 
-def greeting
-
+def greeting(greet ,name )#= "Bobby")
+  puts  greet  + " "+ name + "!"
 end
 
-def return_a_value
-
+def return_a_value(k = "Nice" )
+  return k
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(k = "expert")
+  return k
 end
 
-def pizza_party
-
+def pizza_party(k = "cheese")
+  return k
 end
